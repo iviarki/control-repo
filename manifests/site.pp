2da377606f6fb5cab46c78ae5a548b3016147481
Mark Tomzynski <tomzynsk@amazon.com>
2018-04-03 09:43:52 +0200

    Choco added

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,7 +26,11 @@ File { backup => false }
 # specified in the console for that node.
 
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+  class role::choco_firefox { 
+    include chocolatey 
+    package { 'firefox': 
+      ensure => installed, 
+      provider => 'chocolatey', 
+    } 
+  } 
 }
